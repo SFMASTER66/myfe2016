5a7a74cf3fae848b888f580513e232fd3067ac2f
SFMASTER <705614970@qq.com>
2016-12-12 22:53:51 +0800

    node test2

---
+++ node/test2.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 你大爷 on 2016/12/12.
+ */
+//exports.xx=function(){       //这个exports是必须的，意思就是让xx暴露出来，然后让引入这个文件的文件可以调用这个xx方法。
+//    console.log("i love you");
+//}
+
+//exports.aa=function(arr){
+//    function arrs(a,b){
+//        return a-b;
+//    }
+//    console.log(arr.sort(arrs));
+//}
