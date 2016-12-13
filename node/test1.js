f825913f3a8dd1f8bc6873f005b467e80187f924
SFMASTER <705614970@qq.com>
2016-12-13 16:36:38 +0800

    node中require括号里面引进的js和events的关键字 引进js可以都文件 引进events可以用emit发送东西。

--- node/test1.js
+++ node/test1.js
@@ -9,10 +9,36 @@
 //var array=[1,3,54,2,335,678,55,3];
 //bb.aa(array);w
 
-var http=require("http");
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    //res.writeHead("i love you");
-    res.end('Hello World.i really love you\n');
-}).listen(3000);
-console.log("the server is running at the port 23232000");
+//var http=require("http");     //这个HTTP是node自带就有的
+//http.createServer(function(req,res){
+//    res.writeHead(200,{'Content-Type':'text/plain'});
+//    res.write("i love you\n");
+//    res.end('Hello World.i really love you\n');
+//}).listen(3000);
+//console.log("the server is running at the port 23232000");
+
+//var ss=require("fs");   //这个fs有点像关键字，换成别的好像还不好使，算是node自带的吧
+//ss.readFile("text","utf-8",function(err,data){
+//    if(err){
+//        console.error(err);    //什么时候会输出err这个错误啊？？
+//    }
+//    else{
+//        console.log(data);
+//    }
+//})
+//console.log('end.');
+
+var aa=require("events").EventEmitter;   //cvents也算是node自带的
+var bb=new aa();
+bb.on("cc",function(){    //bb注册了cc这个监听器，然后通过下面的定时器通过bb发送这个事件监听器，此时就会调用cc这个监听器后面的回调函数了
+    console.log("1111");
+})
+
+var timer=setTimeout(function(){
+    bb.emit("cc");
+},1000)
+
+
+
+
+
