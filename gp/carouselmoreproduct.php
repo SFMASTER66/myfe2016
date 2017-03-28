2bc6496af9eaff8001b135f72fc4247ef7ef54e1
SFMASTER <705614970@qq.com>
2017-03-28 23:07:20 +0800

    carouselmoreproduct.php

---
+++ gp/carouselmoreproduct.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="carousel-inner">
+
+    <div class="item  active">
+
+        <div class="pro-list " data-id="1430">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1430&amp;type=Frame">
+                    <!--                                    <img class="normal lazy" data-original="//wallwa.com/ic/item/1430/200/12e94eafad5b8e641537b5b32d0205fd-800x800.png" src="images/mingxingchanpinyoukuang/12e94eafad5b8e641537b5b32d0205fd-800x800.png" style="display: inline;">-->
+                    <img src="images/submitWork/8686656d4b5ffd187a929db7d13e23f1-2000x2000.png" class="backgroundfanbubao">
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy fanbubao">';
+                    ?>
+
+                </a>
+            </div>
+        </div>
+
+        <div class="pro-list " data-id="1429">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1429&amp;type=Frame">
+                    <!--                                    <img class="normal lazy" data-original="//wallwa.com/ic/item/1429/200/6ff98d8435e5fd1dd54390657709e666-800x800.png" src="images/mingxingchanpinyoukuang/6ff98d8435e5fd1dd54390657709e666-800x800.png" style="display: inline;">-->
+                    <img src="images/submitWork/07180040d18cb0a5205499cd83d5994d-2000x2000.png" class="backgroundbaozhen">
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy baozhen">';
+                    ?>
+                </a>
+            </div>
+        </div>
+
+        <div class="pro-list " data-id="1396">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1396&amp;type=Frame">
+                    <!--                                    <img class="normal lazy" data-original="//wallwa.com/ic/item/1396/200/4944d28d3d7f537d1b1b8c99e8114fa5-800x800.png" src="images/mingxingchanpinyoukuang/4944d28d3d7f537d1b1b8c99e8114fa5-800x800.png" style="display: inline;">-->
+                    <img src="images/submitWork/1697f496a517fb3600512f5b8231bf4f-2000x2000.png" class="backgroundshoujike">
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy shoujike">';
+                    ?>
+                </a>
+            </div>
+        </div>
+
+        <div class="pro-list  last " data-id="1395">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1395&amp;type=Frame">
+                    <!--                                    <img class="normal lazy" data-original="//wallwa.com/ic/item/1395/200/2aac3666088a44c53fba3584273b8f76-800x800.png" src="images/mingxingchanpinyoukuang/2aac3666088a44c53fba3584273b8f76-800x800.png" style="display: inline;">-->
+                    <img src="images/submitWork/60cd3f332a872dac0be4c8c59553494e-1700x1700.png" class="backgroundbeizi">
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy beizi">';
+                    ?>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="item ">
+        <div class="pro-list " data-id="1430">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1430&amp;type=Frame">
+                    <!--                                <img class="normal lazy" data-original="//wallwa.com/ic/item/1430/200/12e94eafad5b8e641537b5b32d0205fd-800x800.png" src="images/mingxingchanpinyoukuang/12e94eafad5b8e641537b5b32d0205fd-800x800.png" style="display: inline;">-->
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy">';
+                    ?>
+                    <span class="intro">有框画</span>
+                </a>
+            </div>
+        </div>
+        <div class="pro-list " data-id="1429">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1429&amp;type=Frame">
+                    <!--                                <img class="normal lazy" data-original="//wallwa.com/ic/item/1429/200/6ff98d8435e5fd1dd54390657709e666-800x800.png" src="images/mingxingchanpinyoukuang/6ff98d8435e5fd1dd54390657709e666-800x800.png" style="display: inline;">-->
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy">';
+                    ?>
+                    <span class="intro">布艺画</span>
+                </a>
+            </div>
+        </div>
+        <div class="pro-list " data-id="1396">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1396&amp;type=Frame">
+                    <!--                                <img class="normal lazy" data-original="//wallwa.com/ic/item/1396/200/4944d28d3d7f537d1b1b8c99e8114fa5-800x800.png" src="images/mingxingchanpinyoukuang/4944d28d3d7f537d1b1b8c99e8114fa5-800x800.png" style="display: inline;">-->
+                    <img src="images/submitWork/ebf4892584b847ce8534aaaa4b64097b-2000x2000.png" class="backgroundertong">
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy ertong">';
+                    ?>
+                </a>
+            </div>
+        </div>
+        <div class="pro-list  last " data-id="1395">
+            <div class="pro-pic text-center">
+                <a target="_top" href="http://wallwa.com/item/product-detail?itemId=1395&amp;type=Frame">
+                    <!--                                <img class="normal lazy" data-original="//wallwa.com/ic/item/1395/200/2aac3666088a44c53fba3584273b8f76-800x800.png" src="images/mingxingchanpinyoukuang/2aac3666088a44c53fba3584273b8f76-800x800.png" style="display: inline;">-->
+                    <img src="images/submitWork/fa5ed0c1139f1b2438258eaeb7e02041-2000x2000.png" class="backgroundnankuan">
+                    <?php
+                    echo '<img src='.$imgaddress.' class="normal lazy nankuan">';
+                    ?>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
